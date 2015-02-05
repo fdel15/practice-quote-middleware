a5fef6994e4b2fa9a5789c478f36762679bb2648
Frank DelPidio <frankdaedalus@gmail.com>
2015-02-04 22:14:03 -0500

    Rspec: update tests to use rickygervais.txt

--- spec/quotes_spec.rb
+++ spec/quotes_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../quotes'
 
 describe Quotes do
-  let( :example ) { Quotes.new() }
+  let( :example ) { Quotes.new("fixtures/rickygervais.txt") }
 
   it 'should be initialized as an array of quotes' do
     expect( example.quotes.kind_of?(Array)).to be true
