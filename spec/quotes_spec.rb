43568fda2c7b1b3c170037ca3132b2b4faa1b3e6
Frank DelPidio <frankdaedalus@gmail.com>
2015-02-04 22:08:25 -0500

    Rspec: it should return a different quote each time random is called

--- spec/quotes_spec.rb
+++ spec/quotes_spec.rb
@@ -1,13 +1,19 @@
 require_relative '../quotes'
 
 describe Quotes do
-  let(:example) { Quotes.new()}
+  let( :example ) { Quotes.new() }
 
   it 'should be initialized as an array of quotes' do
-    expect(example.quotes.kind_of?(Array)).to be true
+    expect( example.quotes.kind_of?(Array)).to be true
   end
 
   it 'should return one random quote' do
-    expect(example.random.count).to eq(1)
+    expect( example.random.count ).to eq(1)
+  end
+
+  it 'should return a different quote each time random is called' do
+    quote1 = example.random
+    quote2 = example.random
+    expect( quote1 == quote2 ).to be false
   end
 end
