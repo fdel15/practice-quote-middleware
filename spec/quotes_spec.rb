5dceaa5c038b145a00863e2f41b1627cd0a03d7a
Frank DelPidio <frankdaedalus@gmail.com>
2015-02-04 21:42:08 -0500

    Rspec: it should return one random quote

--- spec/quotes_spec.rb
+++ spec/quotes_spec.rb
@@ -6,4 +6,8 @@ describe Quotes do
   it 'should be initialized as an array of quotes' do
     expect(example.quotes.kind_of?(Array)).to be true
   end
+
+  it 'should return one random quote' do
+    expect(example.random.count).to eq(1)
+  end
 end
